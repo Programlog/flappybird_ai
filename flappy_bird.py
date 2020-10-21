b85d73e1437191b46ca6b706af8be857c7a251d9
FlashCoder <varunk3249@gmail.com>
2020-10-21 14:51:04 -0400

    added draw methods to birdclass

--- flappy_bird.py
+++ flappy_bird.py
@@ -53,5 +53,25 @@ class Bird:
         else:
             if self.tilt > - 90:
                 self.tilt -= self.ROT_VEL
-                print('hi')
 
+    def draw(self, win):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME * 2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME * 3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME * 4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME * 4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        if self.tilt <= - 80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
