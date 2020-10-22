4ca19cd7a01eacc23fe4a1d12004a1c972f3ae63
FlashCoder <varunk3249@gmail.com>
2020-10-22 13:44:52 -0400

    A commit!

--- flappy_bird.py
+++ flappy_bird.py
@@ -127,6 +127,34 @@ class Pipe:
         return False
 
 
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width
+    IMG = BASE_IMG
+
+    def __init__(self, y):
+        self.x1 = 0
+        self.x2 = self.WIDTH 
+        self.y = y
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x1, self.y))
+
+
+
+
+
 
 def draw_window(win, bird):
     win.blit(BG_IMG, (0,0))
